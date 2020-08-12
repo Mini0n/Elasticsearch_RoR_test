2db9983ac8debf822f4187b158acce67c3360320
Eduardo PL <mini0n.000@gmail.com>
2020-08-12 00:13:39 -0500

    song model data to ES indexing

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
 class Song < ApplicationRecord
   belongs_to :band
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  def as_indexed_json(options = {}){
+    self.as_json(
+      only: [:id, :title, :album, :genre, :year],
+      include: {
+        band: {
+          only: [:name, :country]
+        }
+      }
+    )
+  }
 end
