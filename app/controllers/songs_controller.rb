4b7633d5a0e4db31aad5799758283005ec75647a
Eduardo PL <mini0n.000@gmail.com>
2020-08-12 01:55:33 -0500

    search songs through the index

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class SongsController < ApplicationController
+  def index
+    response = Song.__elasticsearch__.search(
+      query: {
+        multi_match: {
+          query: params[:query],
+          fields: ['title', 'band.name', 'band.country', 'genre', 'album']
+        }
+      }
+    ).results
+
+    render json: {
+      results: response.results,
+      total: response.total
+    }
+  end
+end
