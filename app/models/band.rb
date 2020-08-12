1c6f9a9259054462233f552ddc814699d9a3ecfd
Eduardo PL <mini0n.000@gmail.com>
2020-08-12 02:11:18 -0500

    re-index when an band receives changes

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class Band < ApplicationRecord
+  has_many :songs
+
+  after_save :index_songs_in_elasticsearch
+
+  private
+
+  # This should be done as an outside job like a cronned rake or a sidekiq job
+  def index_songs_in_elasticsearch
+    songs.find_each { |song| song.__elasticsearch__.index_document }
+  end
 end
