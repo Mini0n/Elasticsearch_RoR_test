c6641a94f1ea6622b6102450f1da01517434b36c
Eduardo PL <mini0n.000@gmail.com>
2020-08-13 17:14:43 -0500

    updated query to return up to 100 results

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,11 +4,12 @@ class SongsController < ApplicationController
   def index
     response = Song.__elasticsearch__.search(
       query: {
+        # options: { size: 100 },
         query_string: {
           query: params[:query],
           fields: ['title', 'band.name', 'band.country', 'genre', 'album']
         }
-      }
+      }, size: 100 # ensure all results are shown
     ).results
 
     render json: {
